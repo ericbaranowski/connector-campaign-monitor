69998d4d10d6fbad3d1fd5ef11de264cbe243760
Brendan Knowles <brendan@logic.co.nz>
2017-05-24 10:09:50 +1200

    Start code for custom field creation. Need Sergey's assistance as to the best way to handle this

--- services/main.service.js
+++ services/main.service.js
@@ -18,88 +18,121 @@ const IntellibookService = require('./intellibook.service');
  * @param {Function} cb
  */
 function subscribeNewlyRegisteredClients(bookingId, travellers, cb) {
-  waterfall([
-    /**
-     * Get booking data to get a product information
-     *
-     * @param {Function} cb
-     */
-    (cb) => IntellibookService.getBookingData(bookingId, cb),
-    /**
-     * Get product information by booking data
-     *
-     * @param {Object} bookingData
-     * @param {Function} cb
-     */
-      (bookingData, cb) => IntellibookService.getProductData(bookingData.ProductId, (err, productData) => cb(err, bookingData, productData)),
-    /**
-     * Get currently available lists
-     *
-     * @param {Object} bookingData
-     * @param {Object} productData
-     * @param {Function} cb
-     */
-      (bookingData, productData, cb) => cmClient.clients.getLists(cmClient.$clientId, (err, lists) => cb(err, bookingData, productData, lists)),
-    /**
-     * Check whether a list exists and if not, create a new one
-     *
-     * @param {Object} bookingData
-     * @param {Object} productData
-     * @param {Object[]} lists
-     * @param {Function} cb
-     */
-      (bookingData, productData, lists, cb) => {
-      // Iterate through the available lists and return if an existing list is found
-      for (let list of lists) {
-        if (productData.Name === list.Name) {
-          return cb(null, list.ListID, bookingData, productData);
-        }
-      }
+    waterfall([
+        /**
+         * Get booking data to get a product information
+         *
+         * @param {Function} cb
+         */
+            (cb) => IntellibookService.getBookingData(bookingId, cb),
+        /**
+         * Get product information by booking data
+         *
+         * @param {Object} bookingData
+         * @param {Function} cb
+         */
+            (bookingData, cb) => IntellibookService.getProductData(bookingData.ProductId, (err, productData) => cb(err, bookingData, productData)),
+        /**
+         * Get currently available lists
+         *
+         * @param {Object} bookingData
+         * @param {Object} productData
+         * @param {Function} cb
+         */
+            (bookingData, productData, cb) => cmClient.clients.getLists(cmClient.$clientId, (err, lists) => cb(err, bookingData, productData, lists)),
+        /**
+         * Check whether a list exists and if not, create a new one
+         *
+         * @param {Object} bookingData
+         * @param {Object} productData
+         * @param {Object[]} lists
+         * @param {Function} cb
+         */
+            (bookingData, productData, lists, cb) => {
+            // Iterate through the available lists and return if an existing list is found
+            for (let list of lists) {
+                if (productData.Name === list.Name) {
+                    return cb(null, list.ListID, bookingData, productData);
+                }
+            }
 
-      // Create a new list
-      cmClient.lists.createList(
-        cmClient.$clientId,
-        {
-          Title: productData.Name,
-          UnsubscribePage: '',
-          UnsubscribeSetting: 'OnlyThisList',
-          ConfirmedOptIn: false,
-          ConfirmationSuccessPage: ''
+            // Create a new list
+            cmClient.lists.createList(
+                cmClient.$clientId,
+                {
+                    Title: productData.Name,
+                    UnsubscribePage: '',
+                    UnsubscribeSetting: 'OnlyThisList',
+                    ConfirmedOptIn: false,
+                    ConfirmationSuccessPage: ''
+                },
+                (err, list) => cb(err, list.listId, bookingData, productData)
+            );
         },
-        (err, list) => cb(err, list.listId, bookingData, productData)
-      );
-    },
-    /**
-     * Add subscribers to a list
-     *
-     * @param {string} listId
-     * @param {object} bookingData
-     * @param {object} productData
-     * @param {Function} cb
-     */
-      (listId, bookingData, productData, cb) => {
-      // Generate the required subscribers array
-      const subscribers = travellers.map(t => {
-        return {
-          EmailAddress: t.Email,
-          Name: `${t.FirstName} ${t.LastName}`,
-          CustomFields: [
-            {
-              Key: 'BookingID',
-              Value: t.BookingId,
-            },
-            {
-              Key: 'ProductName',
-              Value: productData.Name,
-            },
-            {
-              Key: 'ProductOptionID',
-              Value: bookingData.ProductOptionId,
-            },
-          ],
-        }
-      });
-      cmClient.subscribers.import(listId, {Subscribers: subscribers}, cb);
-    },
-  ], cb);
+
+        /**
+         * Add custom fields to list if they don't exist
+         *
+         * @param {string} listId
+         * @param {object} bookingData
+         * @param {object} productData
+         * @param {Function} cb
+         */
+            (listId, bookingData, productData, cb) => {
+
+            // TODO: Sergey since custom fields need to be created one at a time (from an internal array) I wasn't sure how best to coordinate this with the outer waterfall.
+            const customFields = [
+                {"FieldName": "Booking ID", "DataType": "Text", "VisibleInPreferenceCenter": false},
+                {"FieldName": "Product Name", "DataType": "Text", "VisibleInPreferenceCenter": false},
+                {"FieldName": "Product Option ID", "DataType": "Number", "VisibleInPreferenceCenter": false}
+            ];
+
+            cmClient.lists.getCustomFields(listId, (err, resp) => {
+
+                var field = customFields[2]; // Should be looping through above array and calling callback once all created (if required)
+
+                var existingField = resp.find(fld => fld.FieldName === field.FieldName );
+                if (existingField)
+                    cb(err, listId, bookingData, productData);
+
+                cmClient.lists.createCustomField(listId, field, (err, resp) => {
+                    cb(err, listId, bookingData, productData);
+                });
+            });
+
+
+        },
+        /**
+         * Add subscribers to a list
+         *
+         * @param {string} listId
+         * @param {object} bookingData
+         * @param {object} productData
+         * @param {Function} cb
+         */
+            (listId, bookingData, productData, cb) => {
+            // Generate the required subscribers array
+            const subscribers = travellers.map(t => {
+                return {
+                    EmailAddress: t.Email,
+                    Name: `${t.FirstName} ${t.LastName}`,
+                    CustomFields: [
+                        {
+                            Key: 'BookingID',
+                            Value: t.BookingId,
+                        },
+                        {
+                            Key: 'ProductName',
+                            Value: productData.Name,
+                        },
+                        {
+                            Key: 'ProductOptionID',
+                            Value: bookingData.ProductOptionId,
+                        },
+                    ],
+                }
+            });
+            cmClient.subscribers.import(listId, {Subscribers: subscribers}, cb);
+        },
+    ], cb);
 }
