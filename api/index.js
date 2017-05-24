7fea1e981456a2774621835bbf5406492645fd0e
Brendan Knowles <brendan@logic.co.nz>
2017-05-25 10:01:03 +1200

    Changes to try and get iisnode working

--- api/index.js
+++ api/index.js
@@ -10,4 +10,7 @@ const MainController = require('../controllers/main.controller');
 function api(app) {
     app.post('/api/subscribe-new-clients', MainController.subscribeNewlyRegisteredClients);
     app.post('/api/unsubscribe-clients', MainController.unsubscribeClients);
+    app.get('/api/info', function(req, res){
+        res.send('hello world');
+    });
 }
